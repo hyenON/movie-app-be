95800be873ec139afa607c26fa4a81925e86800f
hyenON <hy3782@gmail.com>
2025-06-26 14:41:47 +0900

    image

--- src/main/java/org/example/springbe/controller/MainController.java
+++ src/main/java/org/example/springbe/controller/MainController.java
@@ -17,7 +17,7 @@ public class MainController {
     public MoviesDto hello() {
         MoviesDto movies = new MoviesDto();
         MovieDto movieDto = new
-                MovieDto(1,"testMovie","http://54.180.79.104:8080/images/t1.png","summaryTest");
+                MovieDto(1,"testMovie","https://sample-elb-back-18670419.ap-northeast-2.elb.amazonaws.com/images/t1.png","summaryTest");
                 List<MovieDto> moviesList = new ArrayList<>();
         moviesList.add(movieDto);
         movies.setMovies(moviesList);
